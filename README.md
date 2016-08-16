740f78ae819887c0cf931e8885943d4ceb3ef6c7
John Dorlus <jdorlus@mozilla.com>
2016-08-16 17:35:58 -0400

    Added file structure and tabbar class.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# foxpuppet
+# FoxPuppet
+
+Fox Puppet is a library for desktop firefox that provides ui interaction and state.
--- foxpuppet/ui/browser/tabbar.py
+++ foxpuppet/ui/browser/tabbar.py
@@ -2,9 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from foxpuppet.foxpuppet import FoxPuppet
 
-class FoxPuppet(object):
 
-    def __init__(self, selenium):
-        self.selenium = selenium
+class TabBar(FoxPuppet):
+    pass
 
+
+class Tab(FoxPuppet):
+    pass
