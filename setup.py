74d08bb416a1d5dc54e3e8ee20100580b1f1b3b5
jrbenny35 <bennyjr169@gmail.com>
2016-10-20 11:01:41 -0400

    Initial setup file and edited requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='FoxPuppet',
+    version='0.1.0',
+    description='Foxpuppet for Firefox UI testing',
+    long_description='See http://foxpuppet.readthedocs.org/',
+    url='https://github.com/mozilla/FoxPuppet',
+    license='MPL',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pytest',
+        'selenium'
+    ]
+)
