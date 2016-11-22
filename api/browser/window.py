0d518f0d8958d8ca313131ecdc086102feecf8a3
jrbenny35 <bennyjr169@gmail.com>
2016-11-22 10:43:05 -0500

    Added context manager

--- api/browser/window.py
+++ api/browser/window.py
@@ -21,62 +21,21 @@ class BrowserWindow(FoxPuppet):
     def is_private(self):
         """Returns True if this is a Private Browsing window."""
 
-        self.selenium.set_context('chrome')
-        return self.selenium.execute_script("""
-                Components.utils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
+        with self.selenium.context('chrome'):
+            return self.selenium.execute_script("""
+                    Components.utils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 
-                let chromeWindow = arguments[0].ownerDocument.defaultView;
-                return PrivateBrowsingUtils.isWindowPrivate(chromeWindow);
-            """, self.windows.window_element)
-        self.selenium.set_context('content')
-
-    @property
-    def handle(self):
-        return self._handle
-
-    @property
-    def closed(self):
-        """Returns closed state of the chrome window.
-
-        :returns: True if the window has been closed.
-        """
-        return self.handle not in self.selenium.chrome_window_handles
-
-    @property
-    def focused(self):
-        """Returns `True` if the chrome window is focused.
-
-        :returns: True if the window is focused.
-        """
-        self.switch_to()
-
-        return self.handle == self.windows.focused_chrome_window_handle
-
-    def switch_to(self, focus=False):
-        """Switches the context to this chrome window.
-
-        By default it will not focus the window. If that behavior is wanted,
-        the `focus` parameter can be used.
-
-        :param focus: If `True`, the chrome window will be focused.
-
-        :returns: Current window as :class:`BaseWindow` instance.
-        """
-        if focus:
-            self.windows.focus(self.handle)
-        else:
-            self.windows.switch_to(self.handle)
-
-        return self
+                    let chromeWindow = arguments[0].ownerDocument.defaultView;
+                    return PrivateBrowsingUtils.isWindowPrivate(chromeWindow);
+                """, self.windows.window_element)
 
     def open_window(self, private=False):
-        self.selenium.set_context('chrome')
-        self.selenium.find_element(*self._file_menu_button_locator).click()
-        with self.windows.wait_for_new_window():
-            if private:
-                self.selenium.find_element(
-                    *self._file_menu_private_window_locator).click()
-            else:
-                self.selenium.find_element(
-                    *self._file_menu_new_window_button_locator).click()
-        self.selenium.set_context('content')
+        with self.selenium.context('chrome'):
+            self.selenium.find_element(*self._file_menu_button_locator).click()
+            with self.windows.wait_for_new_window():
+                if private:
+                    self.selenium.find_element(
+                        *self._file_menu_private_window_locator).click()
+                else:
+                    self.selenium.find_element(
+                        *self._file_menu_new_window_button_locator).click()
