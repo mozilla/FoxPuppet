663b5d46fa81f6abe77e2ab14c494fa908927233
Dave Hunt <dave.hunt@gmail.com>
2017-01-12 11:32:02 +0000

    Ignore specific flake8 errors for unused imports

--- foxpuppet/windows/__init__.py
+++ foxpuppet/windows/__init__.py
@@ -2,6 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from foxpuppet.windows.base import BaseWindow # noqa
-from foxpuppet.windows.browser import BrowserWindow # noqa
-from foxpuppet.windows.manager import WindowManager # noqa
+from foxpuppet.windows.base import BaseWindow  # noqa: F401
+from foxpuppet.windows.browser import BrowserWindow  # noqa: F401
+from foxpuppet.windows.manager import WindowManager  # noqa: F401
