ea7a71a1c98dd0806fa0c83edaa0fa5e49c0732c
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-01-19 03:22:42 -0800

    Upgrade to support Python 3.6 (#58)
    Fixes #53

--- setup.py
+++ setup.py
@@ -32,4 +32,4 @@ setup(name='FoxPuppet',
           'Topic :: Utilities',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5'])
+          'Programming Language :: Python :: 3.6'])
