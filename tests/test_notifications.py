4efa379b03e3556e8aa9b294a7848598ae1dc54f
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-11-28 11:49:27 -0800

    Added the option to click on an installed extension from the navbar.
    This should allow users to interact with an extension after they
    install it by simply knowing the name.
    
    Since webextensions can not change the URL of the current page,
    FoxPuppet with switch to the new tab that gets opened once the
    extension is clicked. If no new tab is opened the current tab
    will still be selected for interaction.

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -13,68 +13,6 @@ from foxpuppet.windows.browser.notifications.addons import (  # noqa: I001
     AddOnInstallConfirmation)  # noqa: I001
 
 
-@pytest.fixture
-def firefox_options(firefox_options):
-    """Fixture for configuring Firefox."""
-    # Due to https://bugzilla.mozilla.org/show_bug.cgi?id=1329939 we need the
-    # initial browser window to be in the foreground. Without this, the
-    # notifications will not be displayed.
-    firefox_options.add_argument('-foreground')
-    return firefox_options
-
-
-@pytest.fixture
-def addon():
-    """Fixture for creating an installable add-on.
-
-    Returns:
-        :py:class:`AddOn`: Add-on object containing a name and a path to the
-            add-on.
-
-    """
-    class AddOn(object):
-        name = 'WebExtension'
-        path = 'webextension.xpi'
-    return AddOn()
-
-
-@pytest.fixture
-def blocked_notification(addon, browser, webserver, selenium):
-    """Fixture causing a blocked notification to appear in Firefox.
-
-    Returns:
-        :py:class:`AddOnInstallBlocked`: Firefox notification.
-
-    """
-    selenium.get(webserver.url())
-    selenium.find_element_by_link_text(addon.path).click()
-    return browser.wait_for_notification(AddOnInstallBlocked)
-
-
-@pytest.fixture
-def confirmation_notification(browser, blocked_notification):
-    """Fixture that allows an add-on to be installed.
-
-    Returns:
-        :py:class:`AddOnInstallConfirmation`: Firefox notification.
-
-    """
-    blocked_notification.allow()
-    return browser.wait_for_notification(AddOnInstallConfirmation)
-
-
-@pytest.fixture
-def complete_notification(browser, confirmation_notification):
-    """Fixture that installs an add-on.
-
-    Returns:
-        :py:class:`AddOnInstallComplete` Firefox notification.
-
-    """
-    confirmation_notification.install()
-    return browser.wait_for_notification(AddOnInstallComplete)
-
-
 def test_open_close_notification(browser, blocked_notification):
     """Trigger and dismiss a notification."""
     assert blocked_notification is not None
